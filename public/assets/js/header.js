ba75128b5abf2a5cc097b775e11e5a67a3c2de07
Nkduy23 <nguyenkhanhduy23803@gmail.com>
2025-03-15 10:43:44 +0700

    done section new

--- public/assets/js/header.js
+++ public/assets/js/header.js
@@ -1,6 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-    setInterval(() => {
-        let text = document.querySelector(".header__top p");
-        text.innerHTML = text.innerHTML == "Bộ Sưu Tập Đồng Hồ Mới Về" ? "Đăng Ký Nhận Thông Báo Tin Mới" : "Gặp Chuyên Gia Tư Vấn";
-    }, 3000);
-})
+    const textContent = document.querySelector(".header__top p");
+    const messages = [
+        "Bộ Sưu Tập Đồng Hồ Mới Về",
+        "Đăng Ký Nhận Thông Báo Tin Mới",
+        "Gặp Chuyên Gia Tư Vấn"
+    ]
+
+    let index = 0;
+    
+    function changeText() {
+        if (textContent) {
+            index = (index + 1) % messages.length;
+            console.log(index);
+            
+            textContent.textContent = messages[index];
+        }
+    }
+    setInterval(changeText, 3000);
+});
